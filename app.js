ad67464e99f947455e0ed6b4f6824e195f3f40be
Emad Jaber <eng.em.jb@gmail.com>
2021-11-17 23:40:58 +0400

    added the javascript logic

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const subBtn=document.getElementById('submit-btn');
+const errorMsg=document.getElementById('email-error-msg');
+const errorIcn=document.getElementById('email-error-icn');
 const emailField=document.querySelector('.email');
+const emailRegex=/^[a-zA-Z0-9._-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+$/g;
 subBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+
+    
+    if (emailField.value && emailRegex.test(emailField.value)){
+        emailField.value="";
+        errorMsg.classList.remove('show');
+        errorIcn.classList.remove('show');
+
+    }else{
+        errorMsg.classList.add('show');
+        errorIcn.classList.add('show');
+    }
 })
